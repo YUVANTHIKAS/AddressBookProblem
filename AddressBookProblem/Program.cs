c21656df03a3867c90083b9ea6feaead65b88300
Yuvanthika Sarathy <yuvanthika.s@tvsnext.com>
2023-07-17 15:45:55 +0530

    [yuvanthika] Add :created

--- AddressBookProblem/Program.cs
+++ AddressBookProblem/Program.cs
@@ -1,17 +1,35 @@
-﻿using System;
-using static AddressBookProblem.AddressBook;
-
+﻿using AddressBookProblem;
+using System;
 namespace AddressBook
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to AddressBook Problem Statement");
-            CreateAddressBook book = new CreateAddressBook();
-            book.CreateContact();
-
-
+            Console.WriteLine("Enter the option to proceed\n 1.Create Contact\n 2.Edit Contact\n 3.Display Contact\n 4.Exit");
+            bool flag = true;
+            CreateAddressBook createAddressBook = new CreateAddressBook();
+            while (flag)
+            {
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        createAddressBook.CreateContact();
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter the name to edit contact details");
+                        string input = Console.ReadLine();
+                        createAddressBook.EditContact(input);
+                        break;
+                    case 3:
+                        createAddressBook.Display();
+                        break;
+                    case 4:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
