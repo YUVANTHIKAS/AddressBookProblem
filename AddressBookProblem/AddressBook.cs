12241736be0a46db7830f63b8a64e0873a68e35a
Yuvanthika Sarathy <yuvanthika.s@tvsnext.com>
2023-08-11 17:19:40 +0530

    [yuvanthika]Add:implemented

--- AddressBookProblem/AddressBook.cs
+++ AddressBookProblem/AddressBook.cs
@@ -1,25 +1,22 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AddressBook
 {
-    public class AddressBook
+    public class CreateAddressBook
     {
-        Contact contact = new Contact();
-        List<Contact> addressBook = new List<Contact>();
-        Dictionary<string, List<Contact>> dict = new Dictionary<string, List<Contact>>();
 
-        public void getData()
-        {
-            Console.WriteLine("\nEnter your option to proceed:\n1.Create Contact\n2.Add to Dictionary\n3.Edit Contact\n4.Delete Contact\n5.Display\n6.Exit ");
-        }
+        List<Contact> createAddressBook = new List<Contact>();
+        Dictionary<string, List<Contact>> dict = new Dictionary<string, List<Contact>>();
+        int count = 0;
         public void CreateContact()
         {
-            Console.WriteLine("Enter the details\n1.First Name\n2.Last Name\n3.Address\n4.city\n5.State\n6.Zip\n7.Phone Number\n8.Email");
+            Console.WriteLine("Enter the details\n 1.First Name:\n 2.Last Name:\n 3.Address:\n 4.City:\n 5.State:\n" +
+                " 6.Zip:\n 7.Phone Number:\n 8.Email:\n");
             Contact contact = new Contact()
             {
                 FirstName = Console.ReadLine(),
@@ -29,71 +26,83 @@ namespace AddressBook
                 State = Console.ReadLine(),
                 Zip = Console.ReadLine(),
                 PhoneNumber = Console.ReadLine(),
-                Email = Console.ReadLine(),
+                Email = Console.ReadLine()
             };
-            Console.WriteLine("\nEntered details:\n \n" + contact.FirstName + "\n" + contact.LastName + "\n" + contact.Address + "\n" + contact.City + "\n" + contact.State + "\n" + contact.Zip + "\n" + contact.PhoneNumber + "\n" + contact.Email);
-            addressBook.Add(contact);
-            getData();
+            foreach (var data in dict)
+            {
+                foreach (var item in data.Value)
+                {
+                    if (item.FirstName.Equals(contact.FirstName))
+                    {
+                        Console.WriteLine("Name already exists");
+                        count++;
+                    }
+                }
+            }
+            if (count == 0)
+            {
+                Console.WriteLine("The entered details:\n" + "First Name:" + contact.FirstName + '\n'
+                     + "Last Name:" + contact.LastName + '\n' + "Address:" + contact.Address + '\n' + "City:"
+                     + contact.City + '\n' + "State:" + contact.State + '\n' + "Zip:" + contact.Zip + '\n'
+                     + "PhoneNumber:" + contact.PhoneNumber + '\n' + "Email:" + contact.Email + '\n');
+                createAddressBook.Add(contact);
+            }
         }
-
         public void AddAddressBookToDictionary()
         {
-            Console.WriteLine("Adding to dictionary!\n Enter the key you want to add:");
+            Console.WriteLine("Enter the name");
             string uniqueName = Console.ReadLine();
-            dict.Add(uniqueName, addressBook);
-            addressBook = new List<Contact>();
-            getData();
+            dict.Add(uniqueName, createAddressBook);
+            createAddressBook = new List<Contact>();
+        }
+
+        public void AddToJsonFile(string filePath)
+        {
+            var json = JsonConvert.SerializeObject(dict);
+            File.WriteAllText(filePath, json);
         }
         public void EditContact(string name, string contactName)
         {
-
             foreach (var data in dict)
-
             {
                 if (data.Key.Equals(name))
                 {
-                    if (contact.FirstName.Equals(name) || contact.LastName.Equals(name))
+                    foreach (var contact in data.Value)
                     {
-                        Console.WriteLine("Enter the option to Edit\n1.First Name\n2.Last Name\n3.Address\n4.City\n5.State\n6.Zip\n7.Phone Number\n8.Email");
-                        int option = Convert.ToInt32(Console.ReadLine());
-
-                        switch (option)
+                        if (contact.FirstName.Equals(name) || contact.LastName.Equals(name))
                         {
-                            case 1:
-                                contact.FirstName = Console.ReadLine();
-                                break;
-                            case 2:
-                                contact.LastName = Console.ReadLine();
-                                break;
-                            case 3:
-                                contact.Address = Console.ReadLine();
-                                break;
-                            case 4:
-                                contact.City = Console.ReadLine();
-                                break;
-                            case 5:
-                                contact.State = Console.ReadLine();
-                                break;
-                            case 6:
-                                contact.Zip = Console.ReadLine();
-                                break;
-                            case 7:
-                                contact.PhoneNumber = Console.ReadLine();
-                                break;
-                            case 8:
-                                contact.Email = Console.ReadLine();
-                                break;
+                            Console.WriteLine("Enter the option to edit\n 1.Last Name\n 2.Address\n 3.City\n 4.State\n " +
+                                " 5.Zip\n 6.Phone Number\n 7.Email");
+                            int option = Convert.ToInt32(Console.ReadLine());
+                            switch (option)
+                            {
+                                case 1:
+                                    contact.LastName = Console.ReadLine();
+                                    break;
+                                case 2:
+                                    contact.Address = Console.ReadLine();
+                                    break;
+                                case 3:
+                                    contact.City = Console.ReadLine();
+                                    break;
+                                case 4:
+                                    contact.State = Console.ReadLine();
+                                    break;
+                                case 5:
+                                    contact.Zip = Console.ReadLine();
+                                    break;
+                                case 6:
+                                    contact.PhoneNumber = Console.ReadLine();
+                                    break;
+                                case 7:
+                                    contact.Email = Console.ReadLine();
+                                    break;
+                            }
                         }
                     }
                 }
-                else
-                {
-                    Console.WriteLine("No contact has been found");
-                }
-                getData();
             }
         }
-
         public void DeleteContact(string name, string contactName)
         {
             Contact contact = new Contact();
@@ -112,22 +121,61 @@ namespace AddressBook
                 }
                 else
                 {
-                    Console.WriteLine("No dictionary with key exits");
+                    Console.WriteLine("No dictionary with Key Exists");
                 }
             }
-            getData();
-
-
         }
-        public void display()
+        public void Display()
         {
             foreach (var data in dict)
             {
                 Console.WriteLine(data.Key);
-
                 foreach (var contact in data.Value)
                 {
-                    Console.WriteLine(contact.FirstName + "\n" + contact.LastName + "\n" + contact.Address + "\n" + contact.City + "\n" + contact.State + "\n" + contact.Zip + "\n" + contact.PhoneNumber + "\n" + contact.Email);
+                    Console.WriteLine(contact.FirstName + "\n" + contact.LastName + "\n" + contact.Address + "\n" + contact.City
+                        + "\n" + contact.State + "\n" + contact.Zip + "\n" + contact.PhoneNumber + "\n" + contact.Email);
+                }
+            }
+        }
+        public void SearchByCityOrState()
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("1.Search by City\n 2.Search by State\n 3.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the city to search");
+                        string city = Console.ReadLine();
+                        List<Contact> contact = new List<Contact>();
+                        foreach (var data in dict)
+                        {
+                            contact = data.Value.Where(x => x.City.Equals(city)).ToList();
+                            foreach (var Contact in contact)
+                            {
+                                Console.WriteLine(Contact.FirstName + " " + Contact.LastName);
+                            }
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter the State to search");
+                        string state = Console.ReadLine();
+                        List<Contact> contact1 = new List<Contact>();
+                        int count = 0;
+                        foreach (var data in dict)
+                        {
+                            contact1 = data.Value.Where(x => x.State.Equals(state)).ToList();
+                            foreach (var Contact in contact1)
+                            {
+                                Console.WriteLine(Contact.FirstName + " " + Contact.LastName);
+                            }
+                        }
+                        break;
+                    case 3:
+                        flag = false;
+                        break;
                 }
             }
         }
--- AddressBookProblem/Program.cs
+++ AddressBookProblem/Program.cs
@@ -3,49 +3,56 @@ namespace AddressBook
 {
     class Program
     {
+        const string filePath = @"D:\BridgeLabs Training\AddressBook\AddressBook\Addressbook.json";
+
         static void Main(string[] args)
         {
 
-
             bool flag = true;
             string key = null, input = null;
-            AddressBook address = new AddressBook();
-            address.getData();
+            CreateAddressBook createAddressBook = new CreateAddressBook();
             while (flag)
             {
+                Console.WriteLine("Enter the option to proceed\n 1.Create Contact\n 2.Add to Dictionary\n " +
+                "3.Edit Contact\n 4.Delete Contact\n " +
+                "5.Display Contact\n 6.Add to Json\n 7.Search Person in a City or State\n 8.Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
-                        address.CreateContact();
+                        createAddressBook.CreateContact();
                         break;
                     case 2:
-                        address.AddAddressBookToDictionary();
+                        createAddressBook.AddAddressBookToDictionary();
                         break;
                     case 3:
-                        Console.WriteLine("enter key:");
+                        Console.WriteLine("Enter Key");
                         key = Console.ReadLine();
-                        Console.WriteLine("Enter name to edit:");
+                        Console.WriteLine("Enter the name to edit contact details");
                         input = Console.ReadLine();
-                        address.EditContact(key, input);
+                        createAddressBook.EditContact(key, input);
                         break;
                     case 4:
-                        Console.WriteLine("Enter key:");
+                        Console.WriteLine("Enter Key");
                         key = Console.ReadLine();
-                        Console.WriteLine("Enter the name of contact details to be deleted ");
+                        Console.WriteLine("Enter the name of contact details to be deleted");
                         input = Console.ReadLine();
-                        address.DeleteContact(key, input);
+                        createAddressBook.DeleteContact(key, input);
                         break;
                     case 5:
-                        address.display();
+                        createAddressBook.Display();
                         break;
                     case 6:
+                        createAddressBook.AddToJsonFile(filePath);
+                        break;
+                    case 7:
+                        createAddressBook.SearchByCityOrState();
+                        break;
+                    case 8:
                         flag = false;
                         break;
-
                 }
             }
-
         }
     }
 }
